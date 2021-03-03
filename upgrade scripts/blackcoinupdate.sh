952814dd1e2f8fc2c2434e2958b72f51d2399413
oktoshi <9932412+oktoshi@users.noreply.github.com>
2021-03-02 21:55:00 -0600

    blackmore binaries upgrade

---
+++ upgrade scripts/blackcoinupdate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Initializing your Blackcoin binaries upgrade on ROKOS"
+echo "This could take a minute, enjoy some coffee or water and come back soon..."
+
+date
+
+# Download new Blackcoin binaries
+
+wget https://blackcoinmore.org/files/blackcoin-more-armv7-v2.13.2.8-10bb345e7.tar.gz
+
+tar -xzvf blackcoin-more-armv7-v2.13.2.8-10bb345e7.tar.gz
+
+#make sure old binaries are removed to prevent errors
+sudo rm /usr/local/bin/blackmore*
+
+# Copy new binaries to usr local bin
+sudo cp blackmored blackmore-qt blackmore-tx blackmore-cli /usr/local/bin/
+
+# Delete the downloaded files // free space from device
+rm -rf blackcoin-more-armv7-v2.13.2.8-10bb345e7.tar.gz
+rm blackmored blackmore-qt blackmore-tx blackmore-cli
+
+# end Client
+echo "Completed the Blackcoin more binaries upgrade."
+echo "enjoy your ROKOS experience"
+exit 0
